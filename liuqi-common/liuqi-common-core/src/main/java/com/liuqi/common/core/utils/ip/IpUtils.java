7ed813eb07c9d72cb886835d8d088ad4afd15c24
LIUQI <1139642444@qq.com>
2023-01-28 10:59:47 +0800

    优化 获取ip 获取方法

--- liuqi-common/liuqi-common-core/src/main/java/com/liuqi/common/core/utils/ip/IpUtils.java
+++ liuqi-common/liuqi-common-core/src/main/java/com/liuqi/common/core/utils/ip/IpUtils.java
@@ -1,5 +1,6 @@
 package com.liuqi.common.core.utils.ip;
 
+import com.liuqi.common.core.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,37 +21,72 @@ public class IpUtils {
      * @param request 请求对象
      * @return IP地址
      */
-    public static String getIpAddr(HttpServletRequest request){
+    public static String getIpAddr(HttpServletRequest request) {
         log.info("获取客户端IP.......");
-        log.info("获取HttpServletRequest {}",request);
-        if (request==null) { // 获取request 为空
+        log.info("获取HttpServletRequest {}", request);
+        if (request == null) { // 获取request 为空
             return "unknown";
         }
         String ip = null;
         String header = request.getHeader("x-forwarded-for");
-        log.info("获取header {}",header);
-        if(header==null||header.length()==0||"unknown".equalsIgnoreCase(header)){ // 获取header为空
+        log.info("获取header {}", header);
+        if (header == null || header.length() == 0 || "unknown".equalsIgnoreCase(header)) { // 获取header为空
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if(header==null||header.length()==0||"unknown".equalsIgnoreCase(header)){ // 获取header为空
+        if (header == null || header.length() == 0 || "unknown".equalsIgnoreCase(header)) { // 获取header为空
             ip = request.getHeader("X-Forwarded-For");
         }
-        if(header==null||header.length()==0||"unknown".equalsIgnoreCase(header)){ // 获取header为空
+        if (header == null || header.length() == 0 || "unknown".equalsIgnoreCase(header)) { // 获取header为空
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if(header==null||header.length()==0||"unknown".equalsIgnoreCase(header)){ // 获取header为空
+        if (header == null || header.length() == 0 || "unknown".equalsIgnoreCase(header)) { // 获取header为空
             ip = request.getHeader("X-Real-IP");
         }
-        if(header==null||header.length()==0||"unknown".equalsIgnoreCase(header)){ // 获取header为空
+        if (header == null || header.length() == 0 || "unknown".equalsIgnoreCase(header)) { // 获取header为空
             ip = request.getRemoteAddr(); //获取本机ip
         }
-        log.info("ip: {}",ip);
-        if("0:0:0:0:0:0:0:1".equals(ip)){
-            log.info(":::::::::::::");
-        }else {
-            log.info("***********");
+        log.info("ip: {}", ip);
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            log.info(":::::::::::::  {}", ip);
+        } else {
+
+            final String proxyIp = getMultistageReverseProxyIp(ip);
+            log.info("***********  {}", proxyIp);
         }
 
-        return null;
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : getMultistageReverseProxyIp(ip);
     }
+
+
+    /**
+     * 从多级反向代理中获得第一个非unknown IP地址
+     *
+     * @param ip 获得的IP地址
+     * @return 第一个非unknown IP地址
+     */
+    public static String getMultistageReverseProxyIp(String ip) {
+        log.info("AAA   {}", ip.indexOf(","));
+        if (ip != null && ip.indexOf(",") > 0) {
+            String[] ips = ip.trim().split(","); // 分割ip
+            for (String subIp : ips) { //循环遍历ip列表
+                if (false == isUnknown(subIp)) {
+                    ip = subIp;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
+
+    /**
+     * 检测给定字符串是否为未知，多用于检测HTTP请求相关
+     *
+     * @param checkStr 被检测的字符串
+     * @return 是否未知
+     */
+    public static boolean isUnknown(String checkStr) {
+        // 被检测的字符串 为空 或者 unknown 返回true
+        return StringUtils.isBlank(checkStr) || "unknown".equalsIgnoreCase(checkStr);
+    }
+
 }
