87959fe0bb536ace01590333fb7e0e9b987cb965
LIUQI <1139642444@qq.com>
2023-01-28 17:16:04 +0800

    新增 工具类异常

---
+++ liuqi-common/liuqi-common-core/src/main/java/com/liuqi/common/core/exception/UtilException.java
@@ -0,0 +1,24 @@
+package com.liuqi.common.core.exception;
+
+/*
+ *@ClassName UtilException
+ *@Description 工具类异常
+ *@Author LiuQi
+ *@Date 2023/1/28 17:14
+ *@Version 1.0
+ */
+public class UtilException extends RuntimeException {
+    private static final long serialVersionUID = 8247610319171014183L;
+    public UtilException(Throwable throwable){
+        super(throwable.getMessage(),throwable);
+    }
+    public UtilException(String message)
+    {
+        super(message);
+    }
+    public UtilException(String message, Throwable throwable)
+    {
+        super(message, throwable);
+    }
+
+}
