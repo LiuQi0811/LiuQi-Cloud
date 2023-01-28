6a71f4f0db00d2a6f50406fbde15884308713987
LIUQI <1139642444@qq.com>
2023-01-29 07:49:57 +0800

    新增 UUID 工具类随机生成

---
+++ liuqi-common/liuqi-common-core/src/main/java/com/liuqi/common/core/utils/uuid/IdUtils.java
@@ -0,0 +1,23 @@
+package com.liuqi.common.core.utils.uuid;
+
+import lombok.extern.slf4j.Slf4j;
+
+/*
+ *@ClassName IdUtils
+ *@Description id生成器工具类
+ *@Author LiuQi
+ *@Date 2023/1/28 11:30
+ *@Version 1.0
+ */
+@Slf4j
+public class IdUtils {
+
+    /**
+     * 获取随机UUID，使用性能更好的ThreadLocalRandom生成UUID
+     *
+     * @return 随机UUID
+     */
+    public static String fastUUID() {
+        return UUID.fastUUID().toString();
+    }
+}
