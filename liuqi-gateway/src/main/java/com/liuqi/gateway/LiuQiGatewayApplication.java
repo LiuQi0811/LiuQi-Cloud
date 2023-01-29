9ff2380d71bb4f5931c61bfd473db648dd9ebed4
LIUQI <1139642444@qq.com>
2023-01-29 20:34:41 +0800

    新增 网关启动类 服务名称配置相关

---
+++ liuqi-gateway/src/main/java/com/liuqi/gateway/LiuQiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.liuqi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ */
+@SpringBootApplication
+public class LiuQiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiuQiGatewayApplication.class, args);
+    }
+}
