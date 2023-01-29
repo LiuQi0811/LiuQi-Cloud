3da62e78d4320120b077598e32f8a787e6366c17
LIUQI <1139642444@qq.com>
2023-01-29 08:10:46 +0800

    新增 Token的Key常量 权限相关通用常量

---
+++ liuqi-common/liuqi-common-core/src/main/java/com/liuqi/common/core/constant/SecurityConstants.java
@@ -0,0 +1,28 @@
+package com.liuqi.common.core.constant;
+
+/*
+ *@ClassName SecurityConstants
+ *@Description 权限相关通用常量
+ *@Author LiuQi
+ *@Date 2023/1/29 07:58
+ *@Version 1.0
+ */
+public class SecurityConstants
+{
+    /**
+     * 用户标识
+     */
+    public static final String USER_KEY = "user_key";
+
+    /**
+     * 用户id字段
+     */
+    public static final String DETAILS_USER_ID = "user_id";
+
+    /**
+     * 用户名字段
+     */
+    public static final String DETAILS_USERNAME = "username";
+
+
+}
---
+++ liuqi-common/liuqi-common-core/src/main/java/com/liuqi/common/core/constant/TokenConstants.java
@@ -0,0 +1,16 @@
+package com.liuqi.common.core.constant;
+
+/*
+ *@ClassName TokenConstants
+ *@Description Token的Key常量
+ *@Author LiuQi
+ *@Date 2023/1/29 08:05
+ *@Version 1.0
+ */
+public class TokenConstants
+{
+    /**
+     * 令牌秘钥
+     */
+    public final static String SECRET = "abcdefghijklmnopqrstuvwxyz";
+}
